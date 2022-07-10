40abf590bf571a3fc900016585571cb94cb87599
ivirtex <hjozwiakdx@gmail.com>
2022-07-10 18:07:11 -0400

    Bump deps

--- lib/Utils/openUrl.dart
+++ lib/Utils/openUrl.dart
@@ -2,10 +2,11 @@
 
 // Package imports:
 import 'package:url_launcher/url_launcher.dart';
+import 'package:url_launcher/url_launcher_string.dart';
 
 Future<void> openUrl(String url) async {
-  if (await canLaunch(url)) {
-    await launch(url);
+  if (await canLaunchUrlString(url)) {
+    await launchUrlString(url);
   } else {
     throw "Couldn't launch given url!";
   }
